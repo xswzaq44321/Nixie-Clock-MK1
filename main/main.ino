37e191c056bf4ef5ffaa6c81ff35f9065a64be68
IzsKon <eric246871@gmail.com>
2021-04-23 18:35:59 +0800

    Add first working file
    1. show time normally
    2. PWM to adjust nixie tube brightness

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,94 @@
+#include <Wire.h>
+#include "RTClib.h"
+#include "ShiftRegister.h"
+
+#define SET_INTERVAL(TIME, CALLBACK, ...)\
+do {\
+  static unsigned long t = millis();\
+  if(millis() - t >= TIME){\
+    CALLBACK(__VA_ARGS__);\
+    t = millis();\
+  }\
+} while(0)
+
+byte numberToDisplay[IC_COUNT];
+RTC_DS3231 rtc;
+DateTime currentTime;
+
+void timer_setup()
+{
+  // close interrupt
+  cli();
+  
+  // disable compare output mode for OCnA, OCnB, OCnC
+  TCCR1A = 0;
+  TCCR1B = 0;
+//  // CTC mode, clear timer on compare
+//  TCCR1B |= (1 << WGM12);
+  // set prescaler to 1
+  TCCR1B |= (1 << CS10);
+  // timer overflow second formula: 65536 / (16,000,000 / prescaler)
+  // formula: duty_cycle * 65536
+  OCR1A = 1 * 65535;
+  // reset timer counter
+  TCNT1 = 0;
+  // set interrupt mask for TIMER1_COMPA_vect and TIMER1_OVF_vect
+  TIMSK1 = (1 << OCIE1A) | (1 << TOIE1);
+  
+  //open interrupt
+  sei();
+}
+
+ISR(TIMER1_COMPA_vect){
+  emptyDisplay();
+}
+
+ISR(TIMER1_OVF_vect){
+  fastShiftOutArr(numberToDisplay);
+}
+
+/*
+ * update [numberToDisplay] every second
+ * will NOT push data to nixie tube
+ */
+inline void updateTime() {
+  DateTime tmp_t = rtc.now();
+  if (tmp_t.second() != currentTime.second()) {
+    currentTime = tmp_t;
+    
+    #ifdef IC_COUNT_8
+    numberToDisplay[7] = currentTime.hour() / 10;
+    numberToDisplay[6] = currentTime.hour() % 10;
+    numberToDisplay[5] = NO_LIGHT;
+    numberToDisplay[4] = currentTime.minute() / 10;
+    numberToDisplay[3] = currentTime.minute() % 10;
+    numberToDisplay[2] = NO_LIGHT;
+    numberToDisplay[1] = currentTime.second() / 10;
+    numberToDisplay[0] = currentTime.second() % 10;
+    #else
+    numberToDisplay[5] = currentTime.hour() / 10;
+    numberToDisplay[4] = currentTime.hour() % 10;
+    numberToDisplay[3] = currentTime.minute() / 10;
+    numberToDisplay[2] = currentTime.minute() % 10;
+    numberToDisplay[1] = currentTime.second() / 10;
+    numberToDisplay[0] = currentTime.second() % 10;
+    #endif
+  }
+}
+
+inline void rtc_setup() {
+  rtc.begin();
+  currentTime = rtc.now();
+  rtc.adjust(DateTime(2021, 1, 1, 3, 30, 25));
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  timer_setup();
+  
+  register_setup();
+  rtc_setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  SET_INTERVAL(100, updateTime);
 }
---
+++ main/shift_register.h
@@ -0,0 +1,115 @@
+/* our PCB can only have 6 or 8 nixie tubes
+ * define IC_COUNT_8 if there are 8 nixie tubes
+ */
+// #define IC_COUNT_8
+
+#ifdef IC_COUNT_8
+#define IC_COUNT 8
+#else
+#define IC_COUNT 6
+#endif
+
+#define ST_CP 5
+#define SH_CP 6
+#define DS    4
+
+#ifdef __AVR_ATmega32U4__
+
+#define MSDATAHIGH  PORTD |=  (1 << PD4)
+#define MSDATALOW   PORTD &= ~(1 << PD4)
+  
+#define MSCLOCKHIGH PORTD |=  (1 << PD7)
+#define MSCLOCKLOW  PORTD &= ~(1 << PD7)
+
+#define MSLATCHHIGH PORTC |=  (1 << PC6)
+#define MSLATCHLOW  PORTC &= ~(1 << PC6)
+
+#else
+
+#define PORT_DATA   digitalPinToPort(DS)
+#define PORT_CLOCK  digitalPinToPort(SH_CP)
+#define PORT_LATCH  digitalPinToPort(ST_CP)
+
+#define MSDATAHIGH  *portOutputRegister(PORT_DATA)  |=  digitalPinToBitMask(DS)
+#define MSDATALOW   *portOutputRegister(PORT_DATA)  &= ~digitalPinToBitMask(DS)
+
+#define MSCLOCKHIGH *portOutputRegister(PORT_CLOCK) |=  digitalPinToBitMask(SH_CP)
+#define MSCLOCKLOW  *portOutputRegister(PORT_CLOCK) &= ~digitalPinToBitMask(SH_CP)
+
+#define MSLATCHHIGH *portOutputRegister(PORT_LATCH) |=  digitalPinToBitMask(ST_CP)
+#define MSLATCHLOW  *portOutputRegister(PORT_LATCH) &= ~digitalPinToBitMask(ST_CP)
+
+#endif
+
+#define MSDATASET(X) (X) ? (MSDATAHIGH) : (MSDATALOW)
+#define MSCLOCKFLICK do {MSCLOCKHIGH; MSCLOCKLOW;} while (0)
+
+static inline register_setup() {
+  pinMode(ST_CP, OUTPUT);
+  pinMode(SH_CP, OUTPUT);
+  pinMode(DS, OUTPUT);
+}
+
+void fastShiftOutArr(byte data[]) {
+
+  MSLATCHLOW;
+  for (int i = 0; i < IC_COUNT; ++i) {
+
+    /*
+     * set first 4 bits to 0 since data[i]
+     * should always be less than 15
+     */
+    MSCLOCKLOW;
+    MSDATALOW;
+    
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+
+    /* push the remain 4 bits to register */
+    byte bitMask = 0b1000;
+    byte data_cpy = data[i];
+    MSDATASET(data_cpy & bitMask);
+    MSCLOCKFLICK;
+    
+    bitMask >>= 1;
+    MSDATASET(data_cpy & bitMask);
+    MSCLOCKFLICK;
+    
+    bitMask >>= 1;
+    MSDATASET(data_cpy & bitMask);
+    MSCLOCKFLICK;
+    
+    bitMask >>= 1;
+    MSDATASET(data_cpy & bitMask);
+    MSCLOCKFLICK;
+  }
+  MSLATCHHIGH;
+}
+
+void emptyDisplay(){
+  /*
+   * push 0b00001111 to all register
+   * for nixie tube to turn off all light
+   */
+   
+  MSLATCHLOW;
+  for (int i = 0; i < IC_COUNT; ++i) {
+    MSCLOCKLOW;
+    MSDATALOW;
+    
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+
+    MSDATAHIGH;
+    
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+    MSCLOCKFLICK;
+  }
+  MSLATCHHIGH;
+}
